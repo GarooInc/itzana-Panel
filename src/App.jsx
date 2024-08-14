a5c5709e47d278b0ad14cb659a20b6811e305fb4
Jimena Hernández <her21199@uvg.edu.gt>
2024-08-14 12:01:44 -0600

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import './App.css'
+
+function App() {
+
+  return (
+    <div className="App">
+      <header className="App-header">
+        <h1 className='text-6xl font-bold'>Hello World</h1>
+      </header>
+    </div>
+  )
+    
+}
+
+export default App
